2f6f6f6373b76e94f6c097242eabfb76003b845c
batbrain7 <kumar.mohit983@gmail.com>
2017-06-01 10:02:49 +0530

    Initial

---
+++ JASONETTE-Android-master/app/src/main/java/com/jasonette/seed/Component/JasonImageComponent.java
@@ -0,0 +1,162 @@
+package com.jasonette.seed.Component;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.LazyHeaders;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.jasonette.seed.Helper.JasonHelper;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.util.Iterator;
+
+public class JasonImageComponent {
+
+    public static View build(View view, final JSONObject component, final JSONObject parent, final Context context) {
+        if(view == null){
+            try {
+                ImageView imageview;
+                imageview = new ImageView(context);
+                imageview.setAdjustViewBounds(true);
+                return imageview;
+            } catch (Exception e) {
+                Log.d("Error", e.toString());
+                return new View(context);
+            }
+        } else {
+            JasonComponent.build(view, component, parent, context);
+
+            int width = 0;
+            int height = 0;
+            float corner_radius = 0;
+            try {
+                String type;
+                final JSONObject style = JasonHelper.style(component, context);
+                type = component.getString("type");
+
+                // Constructing URL
+                GlideUrl url;
+                LazyHeaders.Builder builder = new LazyHeaders.Builder();
+
+                // Add session if included
+                SharedPreferences pref = context.getSharedPreferences("session", 0);
+                JSONObject session = null;
+                URI uri_for_session = new URI(component.getString("url").toLowerCase());
+                String session_domain = uri_for_session.getHost();
+                if(pref.contains(session_domain)){
+                    String str = pref.getString(session_domain, null);
+                    session = new JSONObject(str);
+                }
+                // Attach Header from Session
+                if(session != null && session.has("header")) {
+                    Iterator<?> keys = session.getJSONObject("header").keys();
+                    while (keys.hasNext()) {
+                        String key = (String) keys.next();
+                        String val = session.getJSONObject("header").getString(key);
+                        builder.addHeader(key, val);
+                    }
+                }
+
+                if(component.has("header")){
+                    Iterator<?> keys = component.getJSONObject("header").keys();
+                    while (keys.hasNext()) {
+                        String key = (String) keys.next();
+                        String val = component.getJSONObject("header").getString(key);
+                        builder.addHeader(key, val);
+                    }
+                }
+
+                url = new GlideUrl(component.getString("url"), builder.build());
+
+                if (style.has("corner_radius")) {
+                    corner_radius = JasonHelper.pixels(context, style.getString("corner_radius"), "horizontal");
+                }
+
+                if (corner_radius == 0) {
+                    try {
+                        if (component.getString("url").matches("\\.gif")) {
+                            Glide
+                                    .with(context)
+                                    .load(url)
+                                    .asGif()
+                                    .into((ImageView)view);
+                        } else {
+                            if(style.has("color")){
+                                Glide
+                                        .with(context)
+                                        .load(url)
+                                        .asBitmap()
+                                        .fitCenter()
+                                        .into(new BitmapImageViewTarget((ImageView)view) {
+                                            @Override
+                                            protected void setResource(Bitmap res) {
+                                                BitmapDrawable d = new BitmapDrawable(context.getResources(), res);
+                                                try {
+                                                    Drawable wrapper = DrawableCompat.wrap(d);
+                                                    DrawableCompat.setTint(wrapper, JasonHelper.parse_color(style.getString("color")));
+                                                    view.setImageDrawable(wrapper);
+                                                } catch (Exception e) {
+                                                    Log.d("Error", e.toString());
+                                                    view.setImageDrawable(d);
+                                                }
+                                            }
+                                        });
+                            } else {
+
+                                Glide
+                                        .with(context)
+                                        .load(url)
+                                        .into((ImageView)view);
+                            }
+
+                        }
+                    } catch (Exception e) {
+                        Log.d("Error", e.toString());
+                    }
+                } else {
+                    final float corner_radius_float = (float)corner_radius;
+                    if (component.getString("url").length() > 0) {
+                        try {
+                            Glide
+                                    .with(context)
+                                    .load(url)
+                                    .asBitmap()
+                                    .fitCenter()
+                                    .into(new BitmapImageViewTarget((ImageView)view) {
+                                        @Override
+                                        protected void setResource(Bitmap res) {
+                                            RoundedBitmapDrawable bitmapDrawable =
+                                                    RoundedBitmapDrawableFactory.create(context.getResources(), res);
+                                            bitmapDrawable.setCornerRadius(corner_radius_float);
+                                            view.setImageDrawable(bitmapDrawable);
+                                        }
+                                    });
+                        } catch (Exception e) {
+                            Log.d("Error", e.toString());
+                        }
+                    }
+                }
+
+                JasonComponent.addListener(view, context);
+                view.requestLayout();
+                return view;
+            } catch (Exception e){
+                Log.d("Error", e.toString());
+            }
+            return new View(context);
+        }
+
+    }
+}
---
+++ JASONETTE-Android-master/app/src/main/java/com/jasonette/seed/Launcher/Launcher.java
@@ -0,0 +1,192 @@
+package com.jasonette.seed.Launcher;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.bumptech.glide.request.target.ViewTarget;
+import com.jasonette.seed.Core.JasonViewActivity;
+import com.jasonette.seed.Helper.JasonHelper;
+import com.jasonette.seed.R;
+
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import static android.R.attr.action;
+
+public class Launcher extends Application {
+    private JSONObject handlers;
+
+    public Launcher() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ViewTarget.setTagId(R.id.glide_request);
+
+        // Look for all extensions and initialize them if they have initialize class methods
+        try {
+            String[] fileList = getAssets().list("file");
+            for(int i = 0 ; i < fileList.length; i++){
+               String filename = fileList[i];
+                String jr = null;
+                try {
+                    InputStream is = getAssets().open("file/" + filename);
+                    int size = is.available();
+                    byte[] buffer = new byte[size];
+                    is.read(buffer);
+                    is.close();
+                    jr = new String(buffer, "UTF-8");
+                    JSONObject jrjson = new JSONObject(jr);
+                    if(jrjson.has("classname")){
+                        String resolved_classname = "com.jasonette.seed.Action." + jrjson.getString("classname");
+
+                        String classmethodName = "initialize";
+                        Class<?> classObject = Class.forName(resolved_classname);
+                        Method classMethod = classObject.getMethod("initialize", Context.class);
+                        classMethod.invoke(classObject, getApplicationContext());
+                    }
+                } catch (Exception e) {
+                }
+            }
+
+            // handler init
+            handlers = new JSONObject();
+
+        } catch (Exception e) {
+            Log.d("Error", e.toString());
+        }
+
+    }
+
+
+    /***************************
+     *
+     *  Intent schedule/trigger
+     *
+     ***************************/
+
+    public void on(String key, JSONObject val){
+        try {
+            JSONObject store = new JSONObject();
+            store.put("type", "on");
+            store.put("content", val);
+            handlers.put(key, store);
+        } catch (Exception e) {
+            Log.d("Error", e.toString());
+        }
+    }
+    public void once(String key, JSONObject val){
+        try {
+            JSONObject store = new JSONObject();
+            store.put("type", "once");
+            store.put("content", val);
+            handlers.put(key, store);
+        } catch (Exception e) {
+            Log.d("Error", e.toString());
+        }
+    }
+    public void trigger(JSONObject intent_to_resolve, JasonViewActivity context) {
+        try {
+            String type = intent_to_resolve.getString("type");
+            if (type.equalsIgnoreCase("success")) {
+                // success
+
+                Object name = intent_to_resolve.get("name");
+
+                JSONObject handler;
+                if(name instanceof String) {
+                    handler = getHandler((String)name);
+                } else {
+                    handler = getHandler(String.valueOf(intent_to_resolve.getInt("name")));
+                }
+
+                Intent intent = (Intent) intent_to_resolve.get("intent");
+
+                String classname = handler.getString("class");
+                classname = "com.jasonette.seed.Action." + classname;
+                String methodname = handler.getString("method");
+
+                Object module;
+                if (context.modules.containsKey(classname)) {
+                    module = context.modules.get(classname);
+                } else {
+                    Class<?> classObject = Class.forName(classname);
+                    Constructor<?> constructor = classObject.getConstructor();
+                    module = constructor.newInstance();
+                    context.modules.put(classname, module);
+                }
+
+                Method method = module.getClass().getMethod(methodname, Intent.class, JSONObject.class);
+                JSONObject options = handler.getJSONObject("options");
+                method.invoke(module, intent, options);
+
+            } else {
+                // error
+                JSONObject handler = ((Launcher) context.getApplicationContext()).getHandler(String.valueOf(intent_to_resolve.getInt("name")));
+                if (handler.has("options")) {
+                    JSONObject options = handler.getJSONObject("options");
+                    JSONObject action = options.getJSONObject("action");
+                    JSONObject event = options.getJSONObject("event");
+                    Context ctxt = (Context) options.get("context");
+                    JasonHelper.next("error", action, new JSONObject(), event, ctxt);
+                }
+            }
+
+            // reset intent_to_resolve
+        } catch (Exception e) {
+
+        }
+    }
+    public void callback(JSONObject handler, String result, JasonViewActivity context){
+        try {
+            String classname = handler.getString("class");
+            classname = "com.jasonette.seed.Action." + classname;
+            String methodname = handler.getString("method");
+
+            Object module;
+            if (context.modules.containsKey(classname)) {
+                module = context.modules.get(classname);
+            } else {
+                Class<?> classObject = Class.forName(classname);
+                Constructor<?> constructor = classObject.getConstructor();
+                module = constructor.newInstance();
+                context.modules.put(classname, module);
+            }
+
+            Method method = module.getClass().getMethod(methodname, JSONObject.class, String.class);
+            //JSONObject options = handler.getJSONObject("options");
+            method.invoke(module, handler, result);
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    // Private
+
+    private JSONObject getHandler(String key){
+        try {
+            // 1. gets the handler
+            JSONObject handler = handlers.getJSONObject(key);
+            if(handler.has("type")){
+                if(handler.getString("type").equalsIgnoreCase("once")){
+                    // "once" is a one time thing (Only triggered once):
+                    // so we de-register it after getting
+                    handlers.remove(key);
+                }
+            }
+            return handler.getJSONObject("content");
+
+        } catch (Exception e) {
+            Log.d("Error", e.toString());
+            return new JSONObject();
+        }
+    }
+
+}
